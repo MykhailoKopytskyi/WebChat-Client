21e90bfa535ada6d0958f4dfd3ad0dfbfb56c249
Misha <kopitskyi.michail.49025@gmail.com>
2024-06-30 19:11:27 +0100

    Regex username change

--- src/config/config.js
+++ src/config/config.js
@@ -41,7 +41,7 @@ const configuration = {
   validation: {
     emailRegExp: new RegExp(/^(?=.{1,320}$)[A-Za-z0-9_.+-]{1,64}@[A-Za-z0-9-]{1,255}(?:\.[A-Za-z]{2,6}){0,2}.*$/),
     passwordRegExp: new RegExp(/^(?=.*[A-Za-z])(?=.*\d).{8,40}$/),
-    usernameRegExp:  new RegExp(/^[A-Za-z][A-Za-z\d]{7,29}$/)
+    usernameRegExp:  new RegExp(/^[A-Za-z].{7,29}$/)
   },
 }
 
